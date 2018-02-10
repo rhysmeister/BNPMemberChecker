690ece477bf2bd2c29a1d2dc958eaf2a2121488e
Rhys Campbell <rhyscampbell@rhyss-macbook-pro.home>
2018-02-10 12:48:47 +0100

    Added args, tidy up

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # BNPMemberChecker
 A simple tool to check as list of names against a leaked list of BNP Members
+
+# Description
+
+This is a simple took to check a text file of names against a MySQL database containing the names of BNP Members.
+
+The BNP or British National Party, is a right-wing facist organisation.
+
+About the data: https://wikileaks.org/wiki/British_National_Party_membership_and_contacts_list,_2007-2008
+
+About the BNP https://en.wikipedia.org/wiki/British_National_Party
+
+# Setup
+
+1. Create a database in MySQL called 'bnp' - mysql> CREATE DATABASE bnp;
+2. Extract the archive - linux> unzip bnp-members.sql.zip
+3. Load the data from the sql dump - linux> mysql bnp -u user -p < bnp-members.sql
+4. Install PyMySQL - linux> python -m pip install PyMySQL
+5. Run the tool - python bnp.py
+
+# Additional
+
+You may wqish to create some indexes on the database to improve performance.
+
+CREATE INDEX idx_fn_ln ON members (first_name(100), last_name(100));
--- bnp.py
+++ bnp.py
@@ -1,18 +1,32 @@
-import pymysql.cursors, json
+import pymysql.cursors, json, argparse
 
-# Connect to the database
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             db='bnp',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+
 count=0
 set_of_last_names = set()
 
+parser = argparse.ArgumentParser(description='Check a text file against a database of BNP Members.')
+parser.add_argument('--file', type=str, default="members.txt", help='The list of BNP Members. Should be one name per line. Format is assumed to be <first_name> <middle name> <last name>. Middle names is optional and is ignored in the check. If an extact match is not found we perform an initial check with the first name and last name. This is indicated in the output')
+parser.add_argument('--host', type=str, default="localhost")
+parser.add_argument('--username', type=str, default="root")
+parser.add_argument('--password', type=str, default="secret")
+parser.add_argument('--db', type=str, default="bnp")
+parser.add_argument('--table', type=str, default="members")
+args = parser.parse_args()
+
+# Connect to the database
+connection = pymysql.connect(host=args.host,
+                             user=args.username,
+                             password=args.password,
+                             db=args.db,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
 try:
 
+
+
     with connection.cursor() as cursor:
-	fp = open('/Users/rhyscampbell/Documents/clause_one_members_20180207.txt', 'r')
+	fp = open(args.file, 'r')
 	line = fp.readline()
 	while line:
 		try:
@@ -25,13 +39,13 @@ try:
 				print("ValueError on {0}".format(line))
         	# Read a single record
 		set_of_last_names.add(last_name.replace("'", "\\'"))
-        	sql = "SELECT * FROM `members` WHERE `first_name`=%s AND last_name=%s"
+        	sql = "SELECT * FROM {0} WHERE `first_name`=%s AND last_name=%s".format(args.table)
         	cursor.execute(sql, (first_name, last_name))
 		result = cursor.fetchone()
-		# Try First Initial 
+		# Try First Initial
 		if result is None:
 			initial = first_name[0]
-                	sql = "SELECT * FROM members WHERE first_name = '{0}.' AND CHAR_LENGTH(first_name) <=2 AND last_name='{1}'".format(initial, last_name.replace("'", "\\'"))
+                	sql = "SELECT * FROM {0} WHERE first_name = '{1}.' AND CHAR_LENGTH(first_name) <=2 AND last_name='{2}'".format(args.table, initial, last_name.replace("'", "\\'"))
                 	cursor.execute(sql)
                 	result = cursor.fetchone()
 		count += 1
@@ -40,10 +54,6 @@ try:
 		if result is not None:
         		print(result)
 		line = fp.readline()
-	ln_str = "','".join(map(str, set_of_last_names))
-	sql = "SELECT last_name, COUNT(*) as cnt FROM members WHERE last_name IN ('{0}') GROUP BY last_name ORDER BY last_name".format(ln_str)
-	print(sql)
-	cursor.execute(sql)
-	print(cursor.fetchall())
+
 finally:
     connection.close()
